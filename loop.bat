355cd8670599b58861a6407d691fee0e980bc8b3
Windows8r <134237377+Windows8r@users.noreply.github.com>
2023-06-27 19:22:54 +0700

    .

--- loop.bat
+++ loop.bat
@@ -1,9 +1,7 @@
 @echo off
-echo RDP by Avishkar Patil
-tasklist | find /i "ngrok.exe" >Nul && goto check || echo "Tidak bisa mendapatkan NGROK tunnel, pastikan NGROK_AUTH_TOKEN benar di Settings> Secrets> Repository secret. Mungkin VM Anda sebelumnya masih berjalan: https://dashboard.ngrok.com/status/tunnels" & ping 127.0.0.1 >Nul & exit
-:check
-ping 127.0.0.1 > nul
+echo Pinging RDP, So RDP Cant Die...
+:spamming
+ping 127.0.0.1 > null
 cls
-echo RDP by Avishkar Patil
-
-goto check
+echo PINGED !!
+goto spamming
